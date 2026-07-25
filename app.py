4e7d2dd8f952b5a9ef8776dc7887eaac50bcdf74
foziye <fozie.sadra7@gmail.com>
2026-07-25 03:42:57 +0330

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,6 @@ def webhook():
     return "ok"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
